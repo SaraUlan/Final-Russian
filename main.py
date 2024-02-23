097bb88b9a56b33a292fecdcc9cdcc1bf42113c9
SaraUlan <220872@astanait.edu.kz>
2024-02-23 17:42:37 +0600

    initial commit

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
 import telebot
 from telebot import types
-from psycopg2 import OperationalError
 
 bot = telebot.TeleBot('1685917442:AAE2S8fI5p_J1rg_P0YoHTP0GaYU4lamtkY')
 
-
 @bot.message_handler(commands=['start'])
-def start(message):
-    connection = psycopg2.connect('users.sql')
-    cur = connection.cursor()
-
-    cur.execute('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(50))')
-    connection.commit()
-    cur.close()
-    connection.close()
-    bot.send_message(message.chat.id, f'Добро пожаловать {message.from_user.first_name}!' )
-    bot.register_next_step_handler(message, user_name)
+def start_messafe(message):
+    bot.send_message(message.chat.id, 'Приветствую вас в нашем уютном уголке мудрости!\n\n 🌟 Здесь ваш дух будет наполняться вдохновением каждый день. Наш Telegram бот готов подарить вам порцию умных цитат, которые будут вас вдохновлять и мотивировать.\n\n Чтобы сделать этот опыт еще ближе к вам, мы предоставляем возможность добавлять свои любимые цитаты. Поделитесь своей мудростью с сообществом, и давайте вместе создадим атмосферу вдохновения!\n\nПрисоединяйтесь, получайте дозу мудрости каждый день и делитесь своим светом с остальными. 🚀✨')
 
 
-@bot.message_handler(commands=['creators'])
-def creators(message):
-    bot.send_message(message.chat.id, 'Акниет Сара')
+@bot.message_handler(commands=['info'])
+def info_func(message):
+    bot.send_message(message.chat.id, "Приветствуем вас в мире, созданном с любовью и страстью к мудрости! 🌟\n\nО Нас:\n\nМы - команда энтузиастов, которые собрались вместе, чтобы создать этот уникальный уголок вдохновения. Позвольте нам представиться:\n\n🚀 Ақниет Мәулен(SE-2204) Ұлан Сара(SE-2203):\n\n Мы те, кто воплощает идеи в реальность, создавая каждый день новый источник мудрости для вас.\n\n🌈 Наша цель - делиться вдохновением и мудростью. Мы стремимся создать пространство, где каждая цитата зажигает искру в ваших сердцах." )
 
-@bot.message_handler(content_types=['text'])
-@bot.message_handler(commands=['Motivation'])
+
+
+@bot.message_handler(commands=['motivation'])
 def buttons(message):
     markup = types.InlineKeyboardMarkup()
 
@@ -31,8 +22,10 @@ def buttons(message):
     btn2 = types.InlineKeyboardButton('Авторы', callback_data='authors')
     markup.row(btn1, btn2)
     btn3 = types.InlineKeyboardButton('Добавить свои цитаты', callback_data='add_quotes')
+    btn4 = types.InlineKeyboardButton('Мои цитаты', callback_data='own_quotes')
     markup.row(btn3)
-    bot.reply_to(message, 'hello choose', reply_markup=markup)
-     
+    markup.row(btn4)
+    bot.reply_to(message, 'Прекрасно! Выбор – это ключ к индивидуальному опыту. 🤔✨\n\nВыберите одну из следующих опций:', reply_markup=markup)
 
-bot.polling(non_stop=True)
+if __name__=='__main__':
+    bot.infinity_polling()
